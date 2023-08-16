c88c5ca2904e13074ca22174d0a7e097a7f94774
Mauri <mauri.monzon91@gmail.com>
2023-08-16 12:26:14 -0300

    Hecho todos los botones

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -8,11 +8,35 @@ export const CalculadoraScreen = () => {
     <View style={styles.calculadoraContainer}>
       <Text style={styles.resultadoPequeno}>1,500.000</Text>
       <Text style={styles.resultado}>1,500.000</Text>
+      {/* Fila de botones */}
       <View style={styles.fila}>
         <BotonCalc text="C" color="#9B9B9B" />
         <BotonCalc text="+/-" color="#9B9B9B" />
         <BotonCalc text="del" color="#9B9B9B" />
         <BotonCalc text="/" color="#FF9427" />
+      </View>
+      <View style={styles.fila}>
+        <BotonCalc text="7" />
+        <BotonCalc text="8" />
+        <BotonCalc text="9" />
+        <BotonCalc text="x" color="#FF9427" />
+      </View>
+      <View style={styles.fila}>
+        <BotonCalc text="4" />
+        <BotonCalc text="5" />
+        <BotonCalc text="6" />
+        <BotonCalc text="-" color="#FF9427" />
+      </View>
+      <View style={styles.fila}>
+        <BotonCalc text="3" />
+        <BotonCalc text="2" />
+        <BotonCalc text="1" />
+        <BotonCalc text="+" color="#FF9427" />
+      </View>
+      <View style={styles.fila}>
+        <BotonCalc text="0" ancho />
+        <BotonCalc text="." />
+        <BotonCalc text="=" color="#FF9427" />
       </View>
     </View>
   );
